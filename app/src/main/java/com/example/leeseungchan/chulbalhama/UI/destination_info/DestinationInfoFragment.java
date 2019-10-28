dc79584d7e64a0377bac0c653eb6b8a970773f92
Lee seung chan <dltmdcks702@gmail.com>
2019-10-28 19:02:24 +0900

    UI main/personal info/map/destination. (#6)
    * Create habit register UI and defalut sqlite schema
    
    The main activity has 4 menus: list, history, srbai, extra
    The main fragment represent the first UI when user open the app.
    The fragment name "fragment_add_habit" is the XML file for habit
    register UI.
    
    * Renual UI: main
    
     main view is consisted by 3 different xml files. app_bar_main.xml is
    made for tool bar and add button,content_main.xml indicate fragment and
    common components for all ui in main activity, and  activity_main.xml is made for containing
    app_bar_main and content_main components.
    
    * Make personal info fragment UI v.1 and UI for test
    
    On nav drawer, make personal info ui including map setting and
    destination setting.
    
    Make test menu on nav drawer for mid demo.

---
+++ app/src/main/java/com/example/leeseungchan/chulbalhama/UI/destination_info/DestinationInfoFragment.java
@@ -0,0 +1,139 @@
+/**
+ * @todo implement drop down
+ */
+package com.example.leeseungchan.chulbalhama.UI.destination_info;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.leeseungchan.chulbalhama.DestinationActivity;
+import com.example.leeseungchan.chulbalhama.MainActivity;
+import com.example.leeseungchan.chulbalhama.R;
+import com.example.leeseungchan.chulbalhama.UI.map.MapAddFragment;
+import com.example.leeseungchan.chulbalhama.UI.personal_info.PersonalInfoFragment;
+
+public class DestinationInfoFragment extends Fragment implements View.OnClickListener {
+
+    private View view;
+    private Bundle bundle;
+
+    public static DestinationInfoFragment newInstance() {
+        return new DestinationInfoFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle saveInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_destination_info, container, false);
+
+        view = v;
+
+        Button destinationSetBtn = v.findViewById(R.id.destination_setting);
+        destinationSetBtn.setOnClickListener(this);
+
+        Button destinationStoreBtn = v.findViewById(R.id.store_destination);
+        destinationStoreBtn.setOnClickListener(this);
+
+        Spinner hour = (Spinner)v.findViewById(R.id.hour);
+        setAdapter(R.array.hour_array, hour);
+        hour.setSelection(((DestinationActivity)getActivity()).getTimeHour());
+        setSpinnerListener(hour,R.id.hour);
+
+        Spinner minute = (Spinner)v.findViewById(R.id.min);
+        setAdapter(R.array.min_array, minute);
+        minute.setSelection(((DestinationActivity)getActivity()).getTimeMin());
+        setSpinnerListener(minute, R.id.min);
+
+        EditText dest_name = v.findViewById(R.id.destination_name);
+        dest_name.setText(((DestinationActivity)getActivity()).getName());
+        setNameListener(dest_name);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        Fragment fg;
+        if (view.getId() == R.id.destination_setting) {
+            fg = new MapAddFragment();
+            if (!fg.isAdded()) {
+                transaction.replace(R.id.nav_host_fragment, fg)
+                        .commitNowAllowingStateLoss();
+            }
+        } else if (view.getId() == R.id.store_destination) {
+            //@todo implement insert operation to "destination" table.
+            getActivity().finish();
+        }
+    }
+
+    private void setAdapter(int id, Spinner spinner) {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                id, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    private void setNameListener(EditText edit){
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // 입력되는 텍스트에 변화가 있을 때
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                ((DestinationActivity)getActivity()).setName(arg0.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // 입력하기 전에
+            }
+        });
+    }
+
+    private void setSpinnerListener(final Spinner spinner, final int spinnerId){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(spinnerId){
+                    case R.id.hour:
+                        String hour = spinner.getItemAtPosition(position).toString();
+                        if(!hour.equals("0"))
+                            ((DestinationActivity)getActivity()).setTimeHour(Integer.parseInt(hour));
+                        break;
+                    case R.id.min:
+                        String min = spinner.getItemAtPosition(position).toString();
+                        if(!min.equals("0"))
+                            ((DestinationActivity)getActivity()).setTimeMin(Integer.parseInt(min));
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/leeseungchan/chulbalhama/UI/test/DBTestFragment.java
@@ -0,0 +1,52 @@
+package com.example.leeseungchan.chulbalhama.UI.test;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.leeseungchan.chulbalhama.R;
+
+public class DBTestFragment extends Fragment implements View.OnClickListener {
+
+
+    public static DBTestFragment newInstance(){
+        return new DBTestFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle saveInstanceState){
+        View v = inflater.inflate(R.layout.test_fragment_db, container, false);
+        Button backBtn = v.findViewById(R.id.back);
+        backBtn.setOnClickListener(this);
+
+        return v;
+    }
+
+    public void onClick(View view) {
+
+        Fragment fg;
+        switch (view.getId()) {
+            case R.id.back:
+                fg = TestFragment.newInstance();
+                if(!fg.isAdded()) {
+                    replaceFragment(fg);
+                }
+                break;
+        }
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction =
+                getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, fragment)
+                .commitNowAllowingStateLoss();
+    }
+
+}
